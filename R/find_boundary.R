af824b1ec9d6acd99a1ca4864e82d72252636a13
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2024-01-12 12:12:15 +0000

    find_boundary: linting

--- R/find_boundary.R
+++ R/find_boundary.R
@@ -1,39 +1,37 @@
-find_boundary <- function(x){
-  N <- length(x)
+find_boundary <- function(x, return_all = FALSE) {
+  obs_num <- length(x)
   sortx <- sort(x)
 
   leftx <- sortx[10]
-  rightx <- sortx[N - 11]
+  rightx <- sortx[obs_num - 11]
   xseq <- seq(leftx, rightx, length.out = 100)
-  J <- length(xseq)
+  model_num <- length(xseq)
 
   prop <- double(2)
   mu <- double(2)
   sigma <- double(2)
-  label <- logical(2)
-  ll <- double(J)
-  comp_mixpdf <- matrix(nrow = N, ncol = 2)
-  for (j in 1:J) {
+  ll <- double(model_num)
+  comp_pdf <- matrix(nrow = obs_num, ncol = 2)
+  for (j in 1:model_num) {
     neg <- x <= xseq[j]
-    prop[1] <- sum(neg) / N
+    prop[1] <- sum(neg) / obs_num
     prop[2] <- 1 - prop[1]
     mu[1] <- mean(x[neg])
     mu[2] <- mean(x[!neg])
     sigma[1] <- stats::sd(x[neg])
     sigma[2] <- stats::sd(x[!neg])
-    # for (k in 1:2) {
-    #   comp_mixpdf[, k] <- prop[k] * dnorm(x, mu[k], sigma[k])
-    # }
-    comp_mixpdf[, 1] <- prop[1] * truncnorm::dtruncnorm(x, -Inf, xseq[j], mu[1], sigma[1])
-    comp_mixpdf[, 2] <- prop[2] * truncnorm::dtruncnorm(x, xseq[j], +Inf, mu[2], sigma[2])
-    ll[j] <- sum(log(rowSums(comp_mixpdf)))
+
+    comp_pdf[, 1] <- truncnorm::dtruncnorm(x, -Inf, xseq[j], mu[1], sigma[1])
+    comp_pdf[, 2] <- truncnorm::dtruncnorm(x, xseq[j], +Inf, mu[2], sigma[2])
+
+    ll[j] <- sum(log(comp_pdf %*% prop))
   }
 
-  ll_max = max(ll)
+  ll_max <- max(ll)
   ll_one <- sum(stats::dnorm(x, mean(x), stats::sd(x), log = TRUE))
 
-  bic_one = 2 * log(N) - 2 * ll_one
-  bic_two = 4 * log(N) - 2 * ll_max
+  bic_one <- 2 * log(obs_num) - 2 * ll_one
+  bic_two <- 4 * log(obs_num) - 2 * ll_max
 
   if (return_all) {
     return(list(ll_vec = ll,
