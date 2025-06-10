c3cf9bb7af0936caacdc0558cb4430c2e8428893
UltanPDoherty <ultanpdoherty@gmail.com>
2025-06-10 18:06:40 +0100

    find_split_2d

--- R/find_split_2d.R
+++ R/find_split_2d.R
@@ -1,23 +1,26 @@
 find_split_2d <- function(
     x, y, positive, mechanism, boundary_noise_comp = TRUE) {
   sign_mult <- ifelse(positive, -1, +1)
-  
+
   theta <- seq(0, pi / 2, length.out = 10)[-c(1, 10)]
   alpha <- tan(theta) / (1 + tan(theta))
-  split_mat <- matrix(nrow = length(theta), ncol = 4)
+  beta <- sign_mult * (1 - alpha)
+  split_mat <- matrix(nrow = length(theta), ncol = 6)
   for (i in seq_along(theta)) {
-    z <- alpha[i] * iris[, 1] + sign_mult * (1 - alpha[i]) * iris[, 2]
+    z <- alpha[i] * x + beta[i] * y
     if (mechanism == "valley") {
       split_mat[i, 1:2] <- find_valley(z)
     } else if (mechanism == "boundary") {
       split_mat[i, 1:2] <- find_boundary(z)
     }
   }
-  split_mat[, 3] <- - split_mat[, 1] / (1 - alpha)
+  split_mat[, 3] <- -split_mat[, 1] / (1 - alpha)
   split_mat[, 4] <- alpha / (1 - alpha)
-  
+  split_mat[, 5] <- alpha
+  split_mat[, 6] <- beta
+
   max_ind <- which.max(split_mat[, 2])
-  
+
   split_mat[max_ind, ]
 }
 
