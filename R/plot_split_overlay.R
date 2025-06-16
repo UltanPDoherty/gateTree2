d0b7350f16c92ef2f4806d332c0e0fc40a96370c
UltanPDoherty <ultanpdoherty@gmail.com>
2025-06-16 18:06:08 +0100

    Create plot_split_overlay

---
+++ R/plot_split_overlay.R
@@ -0,0 +1,124 @@
+#' @title Density plot with a `gateTree` split.
+#'
+#' @description
+#' Plot a univariate density of the data with the `gateTree` split
+#' illustrated, and information about the split in the title and subtitle.
+#'
+#' @inheritParams gatetree
+#' @param gatetree_out Output from gatetree.
+#' @param pop The population number.
+#' @param batch The batch number.
+#' @param samp The sample number.
+#' @param var The variable number.
+#'
+#' @return `ggplot` object.
+#'
+#' @export
+plot_split_overlay <- function(
+    matrices, gatetree_out, var, pop, batch = NULL, samp = NULL) {
+  gatetree_call <- gatetree_out$call
+  gatetree_out <- gatetree_out$output
+
+  if (!is.null(names(gatetree_out[[pop]]$pm_previous))) {
+    var_name <- names(gatetree_out[[pop]]$pm_previous)[var]
+  } else {
+    var_name <- var
+  }
+  if (!is.null(names(gatetree_out))) {
+    pop_name <- names(gatetree_out)[pop]
+  } else {
+    pop_name <- pop
+  }
+
+  if (is.na(gatetree_out[[pop]]$method[var])) {
+    split_num <- max(gatetree_out[[pop]]$order, na.rm = TRUE)
+  } else {
+    split_num <- gatetree_out[[pop]]$order[var]
+  }
+
+  dens_dfs <- list()
+  if (!is.null(var) && !is.null(pop)) {
+    batch_num <- length(matrices)
+    samp_num <- vapply(matrices, length, integer(1L))
+    samp_count <- 1
+    for (b in seq_len(batch_num)) {
+      for (j in seq_len(samp_num[b])) {
+        dens_dfs[[samp_count]] <- dens_single_split(
+          matrices, gatetree_out, gatetree_call,
+          pop, b, j, var, split_num
+        )
+        samp_count <- samp_count + 1
+      }
+    }
+  } else {
+    stop("At least pop and var are required.")
+  }
+
+  batch_names <- names(matrices)
+  samp_names <- lapply(matrices, names)
+  dens_df <- Reduce(rbind, dens_dfs)
+  dens_df$batch_name <- batch_names[dens_df$batch]
+  dens_df$samp_name <- sapply(
+    seq_len(nrow(dens_df)),
+    \(x) samp_names[[c(dens_df$batch[x], dens_df$samp[x])]]
+  )
+
+  dens_x <- counts_y <- samp_name <- NULL
+  gg <- dens_df |>
+    ggplot2::ggplot(ggplot2::aes(
+      x = dens_x, y = counts_y, group = samp_name, colour = samp_name
+    )) +
+    ggplot2::geom_line(show.legend = FALSE) +
+    ggplot2::labs(
+      title = paste0(
+        "Population: ", pop_name, ", ",
+        "Variable: ", var_name
+      ),
+      x = var_name,
+      y = "# of Events"
+    ) +
+    ggplot2::theme_bw()
+
+  if (!all(is.na(dens_df$split_val))) {
+    gg <- gg + ggplot2::geom_vline(xintercept = dens_df$split_val, na.rm = TRUE)
+  }
+
+  min_cut <- gatetree_call$min_cutoffs[var]
+  max_cut <- gatetree_call$max_cutoffs[var]
+  if (is.finite(min_cut)) {
+    gg <- gg + ggplot2::geom_vline(
+      ggplot2::aes(xintercept = min_cut),
+      linetype = "dotted", linewidth = 2, colour = "red"
+    )
+  }
+  if (is.finite(max_cut)) {
+    gg <- gg + ggplot2::geom_vline(
+      ggplot2::aes(xintercept = max_cut),
+      linetype = "dotted", linewidth = 2, colour = "red"
+    )
+  }
+
+  gg
+}
+
+dens_single_split <- function(
+    matrices, gatetree_out, gatetree_call, pop, batch, samp, var, split_num) {
+  x <- matrices[[batch]][[samp]][
+    gatetree_out[[pop]]$subsetter[[batch]][[samp]][, split_num],
+  ]
+
+  x <- x[, var]
+  min_cut <- gatetree_call$min_cutoffs[var]
+  max_cut <- gatetree_call$max_cutoffs[var]
+  x <- x[x > min_cut]
+  x <- x[x < max_cut]
+
+  split_val <- gatetree_out[[pop]]$splits[[batch]][[samp]][var]
+
+  dens <- stats::density(x)
+  dens_x <- dens$x
+  counts_y <- dens$y * length(x)
+  dens_df <- data.frame(batch, samp, dens_x, counts_y, as.numeric(split_val))
+
+  dens_df
+}
