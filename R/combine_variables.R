ef041ee737406eac99a0d267294735f0b80f2b71
UltanPDoherty <ultanpdoherty@gmail.com>
2025-06-20 21:56:19 +0100

    Created reconstruct_2d_line

--- R/combine_variables.R
+++ R/combine_variables.R
@@ -24,3 +24,33 @@ combine_variables <- function(
 
   z
 }
+
+#' Compute the slope and intercept of a two-dimensional line for a univariate
+#' split from a combined variable.
+#'
+#' @inheritParams combine_variables
+#' @param split Value of the split for the combined variable.
+#'
+#' @returns A numeric vector.
+#' @export
+reconstruct_2d_line <- function(
+    split, x, y, degrees, min_max_scaling = NULL) {
+  stopifnot(length(degrees) == 1)
+  stopifnot(length(x) == length(y))
+
+  theta <- degrees * (pi / 180)
+  slope <- ifelse(theta == pi / 2, NA, tan(theta))
+  alpha <- ifelse(is.na(slope), 1, abs(slope / sqrt(1 + slope^2)))
+  beta <- ifelse(is.na(slope), 0, -sign(slope) / sqrt(1 + slope^2))
+
+  z <- alpha * x + beta * y
+
+  if (!is.null(min_max_scaling)) {
+    split <- split * (max(z) / min_max_scaling[2])
+    split <- split + min(z) - min_max_scaling[1]
+  }
+
+  intercept <- split / beta
+
+  data.frame("slope" = slope, "intercept" = intercept)
+}
