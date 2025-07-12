46f3065ad8f81a467ee18c6453b35d0da0873217
UltanPDoherty <ultanpdoherty@gmail.com>
2025-07-12 15:46:24 +0100

    find_peak_edge

--- R/find_peak_edge.R
+++ R/find_peak_edge.R
@@ -8,9 +8,11 @@
 #'
 #' @export
 find_peak_edge <- function(x, after_peak = TRUE) {
-  gmm1 <- Mclust(x, G = 1, modelNames = "E", verbose = FALSE)
+  gmm1 <- densityMclust(
+    x, G = 1, modelNames = "E", verbose = FALSE, plot = FALSE
+  )
 
-  dens1 <- dens(x, modelName = "X", parameters = gmm1$parameters)
+  dens1 <- gmm1$dens
 
   noise1 <- dens1 < hypvol(x, TRUE)
 
@@ -21,9 +23,11 @@ find_peak_edge <- function(x, after_peak = TRUE) {
   )
 
   if (after_peak) {
-    edge <- max(x[gmm1n$classification == 1])
+    after <- x > x[tail(which(dens1 == max(dens1)), 1)]
+    edge <- min(x[after][noise1[after]])
   } else {
-    edge <- min(x[gmm1n$classification == 1])
+    before <- x < x[head(which(dens1 == max(dens1)), 1)]
+    edge <- max(x[before][noise1[before]])
   }
 
   return(edge)
